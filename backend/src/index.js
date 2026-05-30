1f881a4cc0b49f180d014bcb306f9ca824b2bb32
aditya22314 <tejusadianu500@gmail.com>
2026-05-30 15:41:02 +0530

    Production related fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 app.use(express.json({ limit: "10mb" }));
@@ -24,6 +26,14 @@ const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   connectDb();
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL:
+    import.meta.env.MODE == "development"
+      ? "http://localhost:5001/api"
+      : "/api",
   withCredentials: true, // This tells browser to include credentials ( cookies,HTTP auth, client certifcates ) when sending requests
 });
-
